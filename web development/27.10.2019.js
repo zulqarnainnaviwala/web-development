5868abc6c2456bd54e239553d680620ec24b2650
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2019-11-19 16:40:41 +0500

    04:40 PM 19/11/2019

--- web development/27.10.2019.js
+++ web development/27.10.2019.js
@@ -1,40 +1,28 @@
-// // es5
-// // for (let i = 0 ; i<10  ; i++){
-// //     console.log(i);
-// // }
-
-
-// let array = [1,2,3,4,5,6,7,8,9]
-
-// Array.prototype.decimalfy = function(){
-//     for(let i = 0 ; i < this.length ; i++)
-//     {
-//         this[i] = this[i].toFixed(2);   
-//     }
+// es5
+// for (let i = 0 ; i<10  ; i++){
+//     console.log(i);
 // }
 
-// array.decimalfy();
+let array = [1,2,3,4,5,6,7,8,9]
+Array.prototype.decimalfy = function(){
+    for(let i = 0 ; i < this.length ; i++)
+    {this[i] = this[i].toFixed(2);}   }
+
+//index k through value access 
+for (let index in array)
+{
+    console.log('index',index);
+    console.log('value',array[index]);
+}
+
+//direct value access
+for(let value of array){
+    console.log(value)
+}
+array.decimalfy();
 
 
-// //index k through value access 
-// for (let index in array)
-// {
-//     console.log('index',index);
-//     console.log('value',array[index]);
-// }
-
-// //direct value access
-// for(let value of array){
-//     console.log(value)
-// }
-
-
-
-
-
-
-
-
-
-
-
+//direct value access
+for(let value of array){
+    console.log(value)
+}
